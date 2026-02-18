c011d11587d70dedeb2e33e90b76964fda6b58d5
Aditi144537 <aditiagarwal1235@gmail.com>
2026-02-18 13:11:57 +0530

    Add files via upload

---
+++ day18.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter value of k: ");
+    scanf("%d", &k);
+
+    k = k % n;   // Handle k > n
+
+    // Rotate k times
+    for(int i = 0; i < k; i++) {
+        int last = arr[n - 1];
+
+        for(int j = n - 1; j > 0; j--) {
+            arr[j] = arr[j - 1];
+        }
+
+        arr[0] = last;
+    }
+
+    printf("Array after rotation:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
